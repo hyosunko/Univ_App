4841b54a99cccf989b2e77bce431d6243f8d8780
hyosunko <hyosunko@gmail.com>
2019-06-27 14:08:57 -0700

    add student course association

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+students = Student.create(
+  [
+    { name: "John Smith",
+      email: "john.smith@example.com",
+      password: "password"
+    },
+    { name: "Jane Doe",
+      email: "jane.doe@example.com",
+      password: "password"
+    },
+    { name: "Jennifer Wonder",
+      email: "jennifer.wonder@example.com",
+      password: "password"
+    },
+    { name: "Jon Snow",
+      email: "jon.snow@example.com",
+      password: "password"
+    },
+    { name: "Tom Hardy",
+      email: "tom.hardy@example.com",
+      password: "password"
+    }
+  ]
+)
